b82a942ee575a2a61c4dbb0477014b320eb23e67
Leonel Dias <leonel.j.dias@gmail.com>
2025-03-04 21:13:37 +0000

    feat: refactor card components for improved structure and add ChartTooltip component

--- resources/js/pages/dashboard/Index.vue
+++ resources/js/pages/dashboard/Index.vue
@@ -4,6 +4,7 @@ import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
 import Map from '../../components/Map.vue';
 import Chart from '../../components/Chart.vue';
+import BarChart from '../../components/BarChart.vue';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -23,8 +24,8 @@ defineProps<{
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex flex-1 flex-col gap-4 p-4">
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-                <div class="aspect-video rounded-xl bg-muted/50"><Chart /></div>
-                <div class="aspect-video rounded-xl bg-muted/50" />
+                <div class="aspect-video rounded-xl bg-muted/50 p-4"><Chart /></div>
+                <div class="aspect-video rounded-xl bg-muted/50 p-4"><BarChart /></div>
                 <div class="aspect-video rounded-xl bg-muted/50" />
             </div>
             <div class="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min"><Map :ships="ships" /></div>
